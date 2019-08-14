f72b63fb73f4e09fd33533277d5055a7abbe41ef
Yuming Liang <liangyuming@aicrobo.com>
2019-08-14 15:41:33 +0800

    Add code comment in class Submap of submaps.h

--- cartographer/mapping/submaps.h
+++ cartographer/mapping/submaps.h
@@ -56,35 +56,48 @@ inline uint8 ProbabilityToLogOddsInteger(const float probability) {
 // track of how many range data were inserted into it, and sets the
 // 'finished_probability_grid' to be used for loop closing once the map no
 // longer changes.
+// 一个子图，首先要有一个local_pose。这个local_pose可以看做是没有经过全局优化的该submap相对
+// 于世界坐标系的位姿。
+// 一张子图要不停地监视有多少range data（这里的range data我理解的就是点云）被插入到这个submap
+// 中。当没有range data插入时，则设置成员变量finished_为真，然后开始做Loop Closure。
 class Submap {
  public:
+  // 构造函数，只包括一个local_pose
   Submap(const transform::Rigid3d& local_submap_pose)
       : local_pose_(local_submap_pose) {}
   virtual ~Submap() {}
 
+  // 序列化与反序列化
+  // 以下三个是纯虚函数，需要继承它的类去实现它。
   virtual void ToProto(proto::Submap* proto,
                        bool include_probability_grid_data) const = 0;
   virtual void UpdateFromProto(const proto::Submap& proto) = 0;
 
   // Fills data into the 'response'.
+  // 把submap的放入到response的proto流中。方便service中查询submap讯息。
   virtual void ToResponseProto(
       const transform::Rigid3d& global_submap_pose,
       proto::SubmapQuery::Response* response) const = 0;
 
   // Pose of this submap in the local map frame.
+  // 返回成员变量local_pose_，即该submap的位姿。
   transform::Rigid3d local_pose() const { return local_pose_; }
 
   // Number of RangeData inserted.
+  // 返回成员变量num_range_data_，即插入到该Submap中的range data的数量。
   int num_range_data() const { return num_range_data_; }
+  // 设置成员变量num_range_data_
   void set_num_range_data(const int num_range_data) {
     num_range_data_ = num_range_data;
   }
 
   // Whether the submap is finished or not.
+  // 查看布尔型成员变量finished_，即该子图是否还需要更新。
   bool finished() const { return finished_; }
   void set_finished(bool finished) { finished_ = finished; }
 
  private:
+  // 三个成员变量
   const transform::Rigid3d local_pose_;
   int num_range_data_ = 0;
   bool finished_ = false;
