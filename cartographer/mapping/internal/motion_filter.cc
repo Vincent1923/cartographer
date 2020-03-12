eb7b87b015d5412a59fd9fe4a184b20981ef9a83
Yuming Liang <liangyuming@aicrobo.com>
2020-03-12 14:50:39 +0800

    Add code comment in MotionFilter::IsSimilar() of motion_filter.cc
    Add code comment in motion_filter of trajectory_builder_2d.lua

--- cartographer/mapping/internal/motion_filter.cc
+++ cartographer/mapping/internal/motion_filter.cc
@@ -43,11 +43,12 @@ bool MotionFilter::IsSimilar(const common::Time time,
       << "Motion filter reduced the number of nodes to "
       << 100. * num_different_ / num_total_ << "%.";
   ++num_total_;
-  if (num_total_ > 1 &&
-      time - last_time_ <= common::FromSeconds(options_.max_time_seconds()) &&
-      (pose.translation() - last_pose_.translation()).norm() <=
+  // 以下三个阈值定义在配置文件 /src/cartographer/configuration_files/trajectory_builder_2d.lua 中
+  if (num_total_ > 1 &&  // 总的 pose 数大于1
+      time - last_time_ <= common::FromSeconds(options_.max_time_seconds()) &&  // 时间间隔小于给定阈值
+      (pose.translation() - last_pose_.translation()).norm() <=  // 移动距离小于给定阈值
           options_.max_distance_meters() &&
-      transform::GetAngle(pose.inverse() * last_pose_) <=
+      transform::GetAngle(pose.inverse() * last_pose_) <=  // 偏转角度小于给定阈值。已知前后位姿求变换矩阵？
           options_.max_angle_radians()) {
     return true;
   }
