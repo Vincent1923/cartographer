a662db7a8f811f3bcb9d024efd3438289343014e
Yuming Liang <liangyuming@aicrobo.com>
2020-03-12 11:47:21 +0800

    Add code comment in class MotionFilter of motion_filter.h

--- cartographer/mapping/internal/motion_filter.h
+++ cartographer/mapping/internal/motion_filter.h
@@ -27,10 +27,15 @@
 namespace cartographer {
 namespace mapping {
 
+// 从 Proto 流中读取配置文件，设置配置项，主要包括最大时间间隔、最大角度间隔、最大距离间隔等
 proto::MotionFilterOptions CreateMotionFilterOptions(
     common::LuaParameterDictionary* parameter_dictionary);
 
 // Takes poses as input and filters them to get fewer poses.
+// 将 poses 作为输入并对其进行过滤以得到更少的 poses
+// MotionFilter主要作用是对数据进行一下滤波。
+// 当两帧数据的间隔时间，两帧的 Pose 跨过的距离，两帧的 Pose 转过的角度等不超过一定的阈值时，
+// 认为新的数据提供的信息很少，这些数据可以直接舍去。
 class MotionFilter {
  public:
   explicit MotionFilter(const proto::MotionFilterOptions& options);
@@ -38,14 +43,18 @@ class MotionFilter {
   // If the accumulated motion (linear, rotational, or time) is above the
   // threshold, returns false. Otherwise the relative motion is accumulated and
   // true is returned.
+  // 根据预先设置的阈值，如果累积运动超过提前预设的阈值，则返回 false；
+  // 否则返回 true，然后把该数据累加上。
+  // MotionFilter 的作用是判断当前位姿、时间和上一时刻对子图进行更新的位姿、时间的间隔是否满足要求，
+  // 当间隔过小时，判断为 IsSimilar = true,就不进行更新。不是实时对地图进行更新，是隔一段时间或距离进行更新。
   bool IsSimilar(common::Time time, const transform::Rigid3d& pose);
 
  private:
-  const proto::MotionFilterOptions options_;
-  int num_total_ = 0;
-  int num_different_ = 0;
-  common::Time last_time_;
-  transform::Rigid3d last_pose_;
+  const proto::MotionFilterOptions options_;  // 配置项，主要包括最大时间间隔、最大角度间隔、最大距离间隔等
+  int num_total_ = 0;             // 总共的 pose 数
+  int num_different_ = 0;         // 过滤之后剩下的 pose 数
+  common::Time last_time_;        // 上一次时间
+  transform::Rigid3d last_pose_;  // 上一次的 pose
 };
 
 }  // namespace mapping
