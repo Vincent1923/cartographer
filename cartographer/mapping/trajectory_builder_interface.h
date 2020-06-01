75860f91847c0105e2e0cf4ddda3682174114f1a
Yuming Liang <liangyuming@aicrobo.com>
2020-06-01 12:55:15 +0800

    Add code comment in class TrajectoryBuilderInterface of trajectory_builder_interface.h

--- cartographer/mapping/trajectory_builder_interface.h
+++ cartographer/mapping/trajectory_builder_interface.h
@@ -45,35 +45,33 @@ class LocalSlamResultData;
 // global SLAM stack, i.e. local SLAM for initial pose estimates, scan matching
 // to detect loop closure, and a sparse pose graph optimization to compute
 // optimized pose estimates.
-// 该接口用于2D和3D SLAM。实现连接global SLAM，例如用于初始位姿估计的local SLAM，用于闭环检测的scan matching，
+// 该接口用于 2D 和 3D SLAM。实现连接 global SLAM，例如用于初始位姿估计的 local SLAM，用于闭环检测的 scan matching，
 // 以及用于计算优化后的位姿估计的稀疏位姿图优化。
+//
+// 接口类 TrajectoryBuilderInterface 是为了给 2D 和 3D 建图提供一个统一的访问接口。
+// 通过该接口我们可以获得一个完整的 SLAM 技术栈，包括局部建图方法、局部位姿估计、闭环检测方法、以及面向稀疏位姿图的全局优化方法。
 class TrajectoryBuilderInterface {
  public:
-  // InsertionResult 就是用来保存插入 Local Slam 的一个节点的数据结构
+  // 结构体 InsertionResult 描述前端的一次子图更新操作，将传感器的扫描数据插入子图中。
   struct InsertionResult {
-    /*
-     * NodeId 是一个结构体，这个结构体应该是用来存储一帧激光数据插入到 submap 中的结果的。
-     * 在插入时，会为该帧数据分配一个 NodeId，NodeId 定义在“/mapping/id.h”中。
-     * 包含两个部分：一个 int 型的 trajectory_id 和一个 int 型的 node_index，
-     * 它使用唯一的 trajectory ID 和该 trajectory 内节点的从零开始的索引 node_index 的组合唯一地识别一个 trajectory node。
-     */
+    // NodeId 是一个结构体，它有两个字段分别记录了轨迹的索引(trajectory_id)以及一个从零开始计数的节点编号(node_index)。
+    // 根据 NodeId 的定义，我们可以把一条轨迹理解为由若干个节点串联起来的一个数据结构。
     NodeId node_id;
-    /*
-     * TrajectoryNode 是一个结构体，在“/mapping/trajectory_node.h”中定义，
-     * TrajectoryNode::Data 包含了经过处理的一帧传感器数据。
-     */
+    // constant_data 记录了子图更新时在局部地图中的位姿，以及有传感器原始数据转换之后的点云信息。此外还记录了更新子图的时刻和重力方向。
     std::shared_ptr<const TrajectoryNode::Data> constant_data;
-    // 已经建立起来的子图列表
+    // insertion_submaps 则记录了被更新的子图对象
     std::vector<std::shared_ptr<const Submap>> insertion_submaps;
   };
 
   // A callback which is called after local SLAM processes an accumulated
   // 'sensor::RangeData'. If the data was inserted into a submap, reports the
   // assigned 'NodeId', otherwise 'nullptr' if the data was filtered out.
-  // 这个回调函数是当一个 RangeData 数据进来时被调用，主要有5个参数：trajectory_id，
-  // 该帧数据的插入时刻 Time, 插入到 Local Slam 中的 pose，RangeData 数据本身和插入的结果 InsertionResult。
-  // InsertionResult 的结构我们进行了详细地分析。
-  // 如果该帧数据成功插入了 submap 中则返回节点 id:NodeId，否则返回空指针
+  // 在局部 SLAM 处理累积的 "sensor::RangeData" 之后调用的回调函数。
+  // 如果数据插入到子图中，则报告分配的 "NodeId"，否则数据被滤除则报告 "nullptr"。
+  //
+  // 通过关键字 using 和 std::function 定义了回调函数 LocalSlamResultCallback 的类型。
+  // 回调函数有5个参数，分别记录了轨迹索引、更新子图的时间、局部的位姿估计、激光传感器的扫描数据在局部坐标系下的点云信息、子图更新结果。
+  // 在我们的 demo 中最终会调用到 cartographer_ros 中定义的函数 OnLocalSlamResult。
   using LocalSlamResultCallback =
       std::function<void(int /* trajectory ID */, common::Time,
                          transform::Rigid3d /* local pose estimate */,
@@ -115,6 +113,7 @@ class TrajectoryBuilderInterface {
       delete;
 
   // 处理传感器数据的5个纯虚函数
+  // 针对不同的传感器数据，接口类 TrajectoryBuilderInterface 定义了5个 AddSensorData() 接口。
   virtual void AddSensorData(
       const std::string& sensor_id,
       const sensor::TimedPointCloudData& timed_point_cloud_data) = 0;
@@ -130,9 +129,11 @@ class TrajectoryBuilderInterface {
   // Allows to directly add local SLAM results to the 'PoseGraph'. Note that it
   // is invalid to add local SLAM results for a trajectory that has a
   // 'LocalTrajectoryBuilder2D/3D'.
-  // 允许直接将本地 SLAM 结果添加到“PoseGraph”中。请注意，为具有“LocalTrajectoryBuilder2D/3D”
-  // 的轨迹添加本地 SLAM 结果是无效的。
-  // 直接将 Local Slam 的结果添加到 PoseGraph 的函数
+  // 允许直接将 local SLAM 结果添加到 "PoseGraph" 中。请注意，为具有 "LocalTrajectoryBuilder2D/3D"
+  // 的轨迹添加 local SLAM 结果是无效的。
+  //
+  // 提供一个接口 AddLocalSlamResultData() 来直接将一个 Local SLAM 的结果添加到后端位姿图上。
+  // 但是那些使用 LocalTrajectoryBuilder2D/3D 作为前端的对象不能使用该接口。
   virtual void AddLocalSlamResultData(
       std::unique_ptr<mapping::LocalSlamResultData> local_slam_result_data) = 0;
 };
